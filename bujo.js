3400e5fb6d279201c8b972275d4deddfbcc1103d
66zzeu <26114342+66zzeu@users.noreply.github.com>
2020-04-12 18:42:15 +0900

    Create bujo.js

---
+++ bujo.js
@@ -0,0 +1,45 @@
+function on_show(event) {
+    var actions_app_id = kintone.app.getRelatedRecordsTargetAppId('actions');
+    // stayやinfo以外を取得し、
+    var query = 'state not in ("stay", "info")'
+    var params = {
+        'app': actions_app_id,
+        'query': query
+    }
+    kintone_api('/k/v1/records', 'GET', params, function(responce) {
+        var update_records = []
+        // 取得できたレコード群に対して、
+        array_each(responce['records'], function(record) {
+            // 学びを含まないなら無視
+            if(record['tag']['value'].indexOf('学び') < 0) {
+                return;
+            }
+            // 学びを含むならstayにする。
+            update_records.push(
+                {
+                    'id': record['$id']['value'],
+                    'record': {
+                        'state': {'value': 'stay'}
+                    },
+                    'revision': record['$revision']['value'],
+                }
+            );
+        })
+        if(0 < update_records.length)
+        {
+            params = {
+                'app': actions_app_id,
+                'records': update_records,
+            }
+            kintone_api('/k/v1/records', 'PUT', params, function(response) {
+                console.log('actions updated.');
+                console.log(response);
+            })
+        }
+        return event;
+    });
+}
+
+// レコード詳細画面が表示されたら
+kintone.events.on('app.record.detail.show', on_show);
+kintone.events.on('mobile.app.record.detail.show', on_show);
